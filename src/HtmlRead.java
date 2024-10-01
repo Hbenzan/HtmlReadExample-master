fafcbdc1e2b4856086f7f66095ffd7978eb691bc
hayesbenzan <hayes_benzan25@milton.edu>
2024-10-01 09:47:46 -0400

    I wrote out my layout design

--- src/HtmlRead.java
+++ src/HtmlRead.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -7,8 +10,15 @@ public class HtmlRead {
     public static void main(String[] args) {
         HtmlRead html = new HtmlRead();
     }
+    private JTextArea ta; // Text area in the middle
+
+    //adding buttons
+    JButton start = new JButton("Start");
+
 
     public HtmlRead() {
+        ta = new JTextArea();
+        ta.setBounds(300, 5, -100, -100);
 
         try {
             System.out.println();
@@ -35,7 +45,6 @@ public class HtmlRead {
             System.out.println(ex);
             System.out.println("There's an error somewhere try again");
         }
-
     }
-
 }
+
